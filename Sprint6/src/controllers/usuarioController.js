649e1e550db5099dd01651979179d32621e34ff6
alexis-vasquez <alexisjvr08@gmail.com>
2022-09-05 19:52:34 -0300

    LoginUser AV

--- Sprint6/src/controllers/usuarioController.js
+++ Sprint6/src/controllers/usuarioController.js
@@ -1,41 +1,49 @@
-const fs = require('fs');
-const path = require('path');
-const db = require('../../database/models/index');
+const fs = require("fs");
+const path = require("path");
+const db = require("../../database/models/index");
 const Op = db.Sequelize.Op;
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 let usuarioController = {
-    login: (req, res) => {
-        res.render('login', {
-            titulo: 'Login',
-            css: 'estiloLogin.css'
-        });
-    },
+  login: (req, res) => {
+    res.render("login", {
+      titulo: "Login",
+      css: "estiloLogin.css",
+    });
+  },
 
-    loginprocess: (req, res) => {
-        
-        for (let i = 0; i < userData.length; i++) {
-            if (userData[i].userEmail == req.body.usuariologin) {
-                if (bcrypt.compareSync(req.body.contrasenialogin, userData[i].userContraseña)) { 
-                    let usuarioLogeado = {
-                        nombre: userData[i].userNombre,
-                        apellido: userData[i].userApellido,
-                        email: userData[i].userEmail,
-                        avatar: userData[i].userAvatar
-                    }
+  loginprocess: (req, res) => {
+    db.Usuario.findAll().then((usuarios) => {
+      for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i] == req.body.usuariologin) {
+          if (
+            bcrypt.compareSync(
+              req.body.contrasenialogin,
+              usuarios[i].userPassword
+            )
+          ) {
+            let usuarioLogeado = {
+              nombre: usuarios[i].userNombre,
+              apellido: usuarios[i].userApellido,
+              email: usuarios[i].userEmail,
+              avatar: usuarios[i].userAvatar,
+            };
 
-                    req.session.login = usuarioLogeado;
+            req.session.login = usuarioLogeado;
 
-                    if (req.body.recordarme) {
-                        res.cookie('userEmail', req.body.usuariologin, {maxAge: 1000 * 60 * 60 * 24});
-                    }
-
-                    return res.redirect('/');
-                }
+            if (req.body.recordarme) {
+              res.cookie("userEmail", req.body.usuariologin, {
+                maxAge: 1000 * 60 * 60 * 24,
+              });
             }
-        };
 
-        let info = req.body.usuariologin;
+            return res.redirect("/");
+          } 
+        }
+      }
+    });
+    
+    let info = req.body.usuariologin;
 
         res.render('login', {
             error: 'Clave o Email incorrecto',
@@ -43,58 +51,54 @@ let usuarioController = {
             titulo: 'Login',
             css: 'estiloLogin.css'
         })
-    },
+  },
 
-    register: (req, res) => {
-        res.render('register', {
-            titulo: 'Registro',
-            css: 'estiloRegistro.css'
-        });
-    },
+  register: (req, res) => {
+    res.render("register", {
+      titulo: "Registro",
+      css: "estiloRegistro.css",
+    });
+  },
 
-    registerProcess: (req, res) => {
-        let passwordEncriptada = bcrypt.hashSync(req.body.clave, 10);
-        
-        if(req.file)
-            {var userAvatar = req.file.filename
-            }
-        else
-            {var userAvatar = "generic_avatar.jpg";
-            }
-        
-        let usuarioNuevo = {
-            userNombre: req.body.nombre,
-            userApellido:   req.body.apellido,
-            userEmail: req.body.email,
-            userContraseña: passwordEncriptada,
-            userAvatar: userAvatar,
-        }
+  registerProcess: (req, res) => {
+    let passwordEncriptada = bcrypt.hashSync(req.body.clave, 10);
 
-        db.Usuario.create(usuarioNuevo);
-
-        res.redirect('/login');
-    },
-
-    profile: (req, res) =>{
-
-      res.render('profile', {
-            titulo: 'Perfil',
-            css: 'estiloProfile.css'
-        })
-    },
-
-    editprofile: (req, res)=>{
-        res.render('editarPerfil', {
-            titulo: 'Editar perfil',
-            css: 'estiloRegistro.css'
-        });
-    },
-
-    logout: (req, res) => {
-        res.clearCookie('userEmail');
-        req.session.destroy();
-        res.redirect('/');
+    if (req.file) {
+      var userAvatar = req.file.filename;
+    } else {
+      var userAvatar = "generic_avatar.jpg";
     }
-}
+
+    db.Usuario.create({
+      userNombre: req.body.nombre,
+      userApellido: req.body.apellido,
+      userEmail: req.body.email,
+      userPassword: passwordEncriptada,
+      userAvatar: userAvatar,
+    });
+
+    res.redirect("/login");
+  },
+
+  profile: (req, res) => {
+    res.render("profile", {
+      titulo: "Perfil",
+      css: "estiloProfile.css",
+    });
+  },
+
+  editprofile: (req, res) => {
+    res.render("editarPerfil", {
+      titulo: "Editar perfil",
+      css: "estiloRegistro.css",
+    });
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
+    res.redirect("/");
+  },
+};
 
 module.exports = usuarioController;
