601c2ccba9e43613c97b8a288aa5bb39144293bb
curyAilen <cury.ailena@gmail.com>
2022-09-20 20:38:48 -0300

    avances de la validación de registro

--- Sprint7/public/js/registro.js
+++ Sprint7/public/js/registro.js
@@ -1,36 +1,63 @@
-window.addEventListener('load', function() {
-    
+window.addEventListener('load', function () {
+  
     let formularioRegistro = document.querySelector(".form_registro");
-    let userName = document.querySelector('#nombre');    
+    let enviar = document.querySelector(".enviar");
+    let userName = document.querySelector('#nombre');
     let errorNombre = document.querySelector(".errorNombre");
 
-    let userEmail = document.querySelector("#email");
+    let userApellido = document.querySelector('#apellido');
+    let errorApellido = document.querySelector(".errorApellido");
+
+
+    let userEmail = document.querySelector("#email").value;
     let errorEmail = document.querySelector(".errorEmail");
 
     let userPassword = document.querySelector("#clave");
     let errorPassword = document.querySelector(".errorPassword");
     let userReClave = document.querySelector("#re-clave");
-    
+
 
     let userAvatar = document.querySelector('#userprofilephotos');
-    let errorAvatar  = document.querySelector(".errorAvatar");
-    formularioRegistro.addEventListener('submit', function(e) {
-        let errores = [];
-        if(userName.value.length <= 2){
-            errores.push('Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-            console.log('Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-        } ;
-        if(userApellido.value.length <= 2){
-            errores.push('Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-            console.log('Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-        } ;
+    let errorAvatar = document.querySelector(".errorAvatar");
 
+    //formularioRegistro.addEventListener('submit', function (e) {
+        enviar.addEventListener('click', function (e) {
+            e.preventDefault();
+        let errores = [];
+        // ○ NOMBRE Y APELLIDO ■ Obligatorio. ■ Deberá tener al menos 2 caracteres.
+        if (userName.value.length <= 2) {
+            errores.push('NOMBRE - Este campo es obligatorio, debes ingresar al menos 3 carácteres');
+          console.log("error nombre")
+        };
+        if (userApellido.value.length <= 2) {
+            errores.push('APELLIDO - Este campo es obligatorio, debes ingresar al menos 3 carácteres');
+          console.log("error apellido")
+            
+        };
+        // ○ EMAIL ■ Obligatorio. ■ Debe ser valido
+        if (!userEmail.includes("@")) {
+            errores.push('EMAIL - Este campo debe contener un email valido');
+            console.log("error email")
+        };
+        // ○ CONTRASEÑA ■ Obligatorio. ■ Deberá tener al menos 8 caracteres.
+
+        // ○ IMAGEN  ■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
+        let imagenValidacion = inputImagen.files[0]
+
+        if(!imagenValidacion){
+            errores.imagen = 'Debe subir una imagen'
+        } else if (imagenValidacion.type != 'image/jpeg' && imagenValidacion.type != 'image/png' 
+        && imagenValidacion.type != 'image/jpg'){
+            errores.imagen = 'El formato de archivo no es valido'
+        }
+
+        //Mostrar errores
         if (errores.length > 0) {
             e.preventDefault();
             let ulErrores = document.querySelector(".errores ul");
-            for(let i=0;i< errores.length;i++ ){
-            ulErrores.innerHTML += '<li>'+ errores[i] +'</li>'
-            } ;          
+            for (let i = 0; i < errores.length; i++) {
+                ulErrores.innerHTML += '<li>' + errores[i] + '</li>' + '<br>'
+            };
         } else {
             formularioRegistro.submit();
         }
