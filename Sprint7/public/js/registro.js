1523df312193743bec3437d4a0ed53920c8db221
curyAilen <cury.ailena@gmail.com>
2022-09-20 22:46:03 -0300

    Actualización de validación front register

--- Sprint7/public/js/registro.js
+++ Sprint7/public/js/registro.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function () {
-  
+
     let formularioRegistro = document.querySelector(".form_registro");
     let enviar = document.querySelector(".enviar");
     let userName = document.querySelector('#nombre');
@@ -21,46 +21,57 @@ window.addEventListener('load', function () {
     let errorAvatar = document.querySelector(".errorAvatar");
 
     //formularioRegistro.addEventListener('submit', function (e) {
-        enviar.addEventListener('click', function (e) {
-            e.preventDefault();
-        let errores = [];
+    enviar.addEventListener('click', function (e) {
+        e.preventDefault();
+        let errores = {};
+
         // ○ NOMBRE Y APELLIDO ■ Obligatorio. ■ Deberá tener al menos 2 caracteres.
         if (userName.value.length <= 2) {
-            errores.push('NOMBRE - Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-          console.log("error nombre")
+            errores.nombre='NOMBRE - Este campo es obligatorio, debes ingresar al menos 3 carácteres.';
+            console.log("error nombre")
         };
         if (userApellido.value.length <= 2) {
-            errores.push('APELLIDO - Este campo es obligatorio, debes ingresar al menos 3 carácteres');
-          console.log("error apellido")
-            
+            errores.apellido='APELLIDO - Este campo es obligatorio, debes ingresar al menos 3 carácteres.';
+            console.log("error apellido")
+
         };
         // ○ EMAIL ■ Obligatorio. ■ Debe ser valido
         if (!userEmail.includes("@")) {
-            errores.push('EMAIL - Este campo debe contener un email valido');
+            errores.email='EMAIL - Este campo debe contener un email valido.';
             console.log("error email")
         };
         // ○ CONTRASEÑA ■ Obligatorio. ■ Deberá tener al menos 8 caracteres.
+        if (userPassword !== userReClave) {
+            errores.password='CONTRASEÑAS - La contraseña no coincide.';
+            console.log("error contraseña no coincide")
 
+        }else if(userPassword == "" || userReClave == ""){
+            errores.password='CONTRASEÑAS - Debe ingresar una contraseña';
+            console.log("error contraseña vacia")
+
+
+        }
         // ○ IMAGEN  ■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
-        let imagenValidacion = inputImagen.files[0]
+        let imagenValidacion = userAvatar.files[0]
 
-        if(!imagenValidacion){
-            errores.imagen = 'Debe subir una imagen'
-        } else if (imagenValidacion.type != 'image/jpeg' && imagenValidacion.type != 'image/png' 
+        if (imagenValidacion && imagenValidacion.type != 'image/jpeg' && imagenValidacion.type != 'image/png' 
         && imagenValidacion.type != 'image/jpg'){
-            errores.imagen = 'El formato de archivo no es valido'
-        }
+            errores.imagen = 'IMAGEN - El formato de archivo no es valido'
+            console.log("error imagen")
 
-        //Mostrar errores
-        if (errores.length > 0) {
-            e.preventDefault();
-            let ulErrores = document.querySelector(".errores ul");
-            for (let i = 0; i < errores.length; i++) {
-                ulErrores.innerHTML += '<li>' + errores[i] + '</li>' + '<br>'
-            };
-        } else {
-            formularioRegistro.submit();
         }
-    });
+        
+        //Mostrar errores
+        if (Object.keys(errores).length >= 1) {
+            errorNombre.innerText =  (errores.nombre) ? errores.nombre : '';
+            errorApellido.innerText =  (errores.apellido) ? errores.apellido : '';
+            errorEmail.innerText = (errores.email) ? errores.email : '';
+            errorPassword.innerText = (errores.password) ? errores.Password : '';
+            errorAvatar.innerText = (errores.imagen) ? errores.imagen : '';
+        } else {
+            formCreate.submit();
+        }
+        });
+
 
 });
