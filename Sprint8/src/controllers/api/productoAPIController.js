b2a731fd45419dfa7c2943df65ea32420d89b84e
julio Tuozzo <jtuozzo@hotmail.com>
2022-10-02 23:03:22 -0300

    En el count de categorías

--- Sprint8/src/controllers/api/productoAPIController.js
+++ Sprint8/src/controllers/api/productoAPIController.js
@@ -1,16 +1,35 @@
 const db = require('../../../database/models/index');
 const Op = db.Sequelize.Op;
+const sequelize = require('sequelize');
 
 let productoAPIController = {
 
     'listado': (req, res) => {
+
         db.Producto.findAll({
+            
+            include: ['categoria'],
+            
+            attributes: [sequelize.col('categoria.categoria'),
+                [sequelize.fn('count', 'id_producto'),'cuantasCategorias']],
+            group: ["fk_id_categoria"]
+        })
+
+            .then(function (categorias) {
+                //console.log(categorias);
+                console.log(categorias[0]);
+
+                res.json(categorias);
+            });
+
+
+        /*db.Producto.findAll({
             include: ['talles', 'categoria']
         })
             .then(productos => {
 
                 for (let i = 0; i < productos.length; i++) {
-                    productos[i].dataValues.imagen= 'http://localhost:3000/images/' + productos[i].dataValues.imagen;
+                    productos[i].dataValues.imagen = 'http://localhost:3000/images/' + productos[i].dataValues.imagen;
                 }
 
                 respuesta = {
@@ -36,15 +55,16 @@ let productoAPIController = {
 
                 res.json(respuesta);
 
-            })
+            })*/
     },
 
     'unProducto': (req, res) => {
         db.Producto.findByPk(req.params.id,
             {
-                include: ['talles']
+                include: ['talles', 'categoria']
             })
             .then(productos => {
+                productos.dataValues.imagen = 'http://localhost:3000/images/' + productos.dataValues.imagen;
                 let respuesta = {
                     meta: {
                         status: 200,
