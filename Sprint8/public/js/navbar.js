1a58f7543f6bf04c97bd30112d3dd977f7c2f1f5
JoacoAnd <joaquinandreone@hotmail.com>
2022-10-05 14:47:15 -0300

    Funcionalidad boton menu para mobile

--- Sprint8/public/js/navbar.js
+++ Sprint8/public/js/navbar.js
@@ -8,4 +8,19 @@ window.addEventListener('load', ()=>{
         }
     });
 
+
+    /* Mostrar menu */
+    const buttonMenu = document.querySelector(".burger-menu");
+    const menu = document.querySelector("ul#barraNavUl");
+
+    buttonMenu.addEventListener("click", (e)=>{
+        e.preventDefault();
+        menu.classList.toggle("mostrarmenu");
+    });
+
+    window.addEventListener("resize", ()=>{
+        if (window.innerWidth > 360 && menu.classList.contains("mostrarmenu")) {
+            menu.classList.remove("mostrarmenu");
+        }
+    })
 })
