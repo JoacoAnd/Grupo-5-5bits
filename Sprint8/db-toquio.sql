8428bc0bee0a283f98cad2ee597e46fa26f3fec0
curyAilen <cury.ailena@gmail.com>
2022-10-06 23:41:09 -0300

    Base de datos con datos nuevos

--- Sprint8/db-toquio.sql
+++ Sprint8/db-toquio.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         127.16.5.10
--- Versión del servidor:         10.4.24-MariaDB - mariadb.org binary distribution
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.17-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             11.3.0.6295
+-- HeidiSQL Versión:             11.2.0.6213
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -13,7 +13,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para toquio
+-- Volcando estructura de base de datos para toquio 
 CREATE DATABASE IF NOT EXISTS `toquio` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `toquio`;
 
@@ -25,7 +25,6 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla toquio.categorias: ~4 rows (aproximadamente)
-DELETE FROM `categorias`;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
 INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
 	(1, 'mujeres'),
@@ -45,22 +44,23 @@ CREATE TABLE IF NOT EXISTS `productos` (
   PRIMARY KEY (`id_producto`),
   KEY `FKcategoria` (`fk_id_categoria`) USING BTREE,
   CONSTRAINT `FKcategoria` FOREIGN KEY (`fk_id_categoria`) REFERENCES `categorias` (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla toquio.productos: ~0 rows (aproximadamente)
-DELETE FROM `productos`;
+-- Volcando datos para la tabla toquio.productos: ~10 rows (aproximadamente)
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `precio`, `imagen`, `fk_id_categoria`) VALUES
-	(1, 'Pantalón Hindú', 'Pantalón de la India', 2500.00, 'img-1657741175182.jpg', 1),
-	(2, 'Pantalón cortito', 'Pantalón para el verano', 500.00, 'img-1657741175182.jpg', 3),
-	(3, 'Pantalón cromado', 'Pantalón de colores', 1000.00, 'img-1657741175182.jpg', 2),
-	(4, 'Remera básica', 'Combian con todo', 750.00, 'img-1657741175182.jpg', 4),
-	(5, 'Remera plateada', 'Remera para ir de fiesta', 10000.00, 'img-1657741175182.jpg', 1),
-	(6, 'Mocasines', 'Mocasines', 2000.00, 'img-1657741175182.jpg', 2),
-	(7, 'Medias 3/4', 'Medias para usar con los mocasines', 300.00, 'img-1657741175182.jpg', 2),
-	(8, 'Cinturón', 'Para que no se caiga el pantalón', 1500.00, 'img-1657741175182.jpg', 2),
-	(9, 'Zapatillas', 'Más cómodas que los mocasines', 7800.00, 'img-1657741175182.jpg', 1),
-	(10, 'Pulsera Griega', 'La pulsera de Zorba', 2000.00, 'img-1657741175182.jpg', 4);
+	(1, 'Jean ', 'Jean estilo roto', 2500.00, 'camisa hombre.jpg', 2),
+	(2, 'Campera jean', 'Campera de jean para niños y niñas', 500.00, 'camisa infante.jpg', 3),
+	(3, 'Buzo ', 'Buzo infantes', 1000.00, 'buzo infante.jpg', 3),
+	(4, 'Buzo amplio', 'Buzo unisex estilo amplio', 750.00, 'buzo hombre.jpg', 4),
+	(5, 'Campera', 'Campera talles pequeños', 10000.00, 'campera infante.jpg', 3),
+	(6, 'Conjunto mujer', 'Conjunto de camisa y pantalon blanco', 2000.00, 'conjunto mujer 2.jpg', 1),
+	(7, 'Cobjunto verde', 'Conjunto de mujer verde, top y pantalon amplio', 30000.00, 'conjunto mujer.jpg', 1),
+	(8, 'Remera blanca ', 'Remera blanca con estampa en el pecho. Amplia', 1500.00, 'remera hombre.jpg', 2),
+	(9, 'Zapato de vestir', 'Más cómodas que los mocasines', 7800.00, 'img-1657741175182.jpg', 2),
+	(10, 'Traje Azul', 'Traje completo para hombre', 2000.00, 'traje hombre.jpg', 2),
+	(14, 'Conjunto blanco', 'Conjunto top y pantalon de mujer blanco', 5000.00, 'top mujer.jpg', 1),
+	(15, 'Vestido Amarillo', 'Vestido mujer, para el verano y primavera', 8477.00, 'vestido mujer.jpg', 1);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 -- Volcando estructura para tabla toquio.producto_talle
@@ -75,8 +75,7 @@ CREATE TABLE IF NOT EXISTS `producto_talle` (
   CONSTRAINT `fk_producto` FOREIGN KEY (`fk_id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla toquio.producto_talle: ~0 rows (aproximadamente)
-DELETE FROM `producto_talle`;
+-- Volcando datos para la tabla toquio.producto_talle: ~31 rows (aproximadamente)
 /*!40000 ALTER TABLE `producto_talle` DISABLE KEYS */;
 INSERT INTO `producto_talle` (`id_producto_talle`, `fk_id_producto`, `fk_id_talle`) VALUES
 	(1, 1, 1),
@@ -120,7 +119,6 @@ CREATE TABLE IF NOT EXISTS `talles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 -- Volcando datos para la tabla toquio.talles: ~5 rows (aproximadamente)
-DELETE FROM `talles`;
 /*!40000 ALTER TABLE `talles` DISABLE KEYS */;
 INSERT INTO `talles` (`id_talle`, `talle`) VALUES
 	(1, 'S'),
@@ -139,14 +137,19 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `userPassword` varchar(255) NOT NULL DEFAULT '0',
   `userAvatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `usuarios` ADD `userRol`
-VARCHAR(5) NULL DEFAULT 'user';
-
--- Volcando datos para la tabla toquio.usuarios: ~0 rows (aproximadamente)
-DELETE FROM `usuarios`;
+-- Volcando datos para la tabla toquio.usuarios: ~8 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` (`id_usuario`, `userNombre`, `userApellido`, `userEmail`, `userPassword`, `userAvatar`) VALUES
+	(1, 'Juan', 'Perez', 'juan.perez@hotmail.com', '4d186321c1a7f0f354b297e8914ab240', 'generic_avatar.jpg'),
+	(2, 'María', 'Doroz', 'mdoroz_1@gmail.com', 'cf1d9217ebc110bcf479dd8b95ece22a', 'generic_avatar.jpg'),
+	(3, 'Pedro José', 'Fernández', 'pfer@gmail.com', '06fb2658f534891b64428d8dcc028738', 'generic_avatar.jpg'),
+	(4, 'Carlos Alberto', 'Jerez', 'jerez_2@hotmail.com', 'b4dc3a2172bfbec00385e378f4eecd56', 'generic_avatar.jpg'),
+	(5, 'Federico', 'Maradona', 'el_diego@gmail.com', 'ee6d605f326b5f58a4fa45d50c1e11e8', 'generic_avatar.jpg'),
+	(6, 'Ana', 'Johansen', 'ana_j@yahoo.com', 'f3242c3db7733c8aff401d5ce5d86e22', 'generic_avatar.jpg'),
+	(7, 'Teresa', 'Beretanie', 'teresa@hotmail.com', 'c46fb06b88af12d51bca8b3e8d68b46b', 'generic_avatar.jpg'),
+	(8, 'Demetria Juana', 'Barthen', 'demedos@gmail.com', '58414aa2076dd4135564f77e112e5a24', 'generic_avatar.jpg');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
